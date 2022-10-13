4d2f6154d10bad4fdfeb1b59f7e14c405d4d8099
Nicola-Matis <nicolamatis@yahoo.com>
2022-10-13 18:16:07 +0300

    .

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //1.
+        System.out.println("Hello");
+        System.out.println("Nicola");
+        //2.
+        byte Calcul = 5 + 5;
+        System.out.println(Calcul);
+        //3.
+        byte Impartire = 100 / 5;
+        System.out.println(Impartire);
+        //4.
+        //A
+        int a = -5;
+        int b = 8;
+        int c = 6;
+        int d = a + b * c;
+        System.out.println(d);
+        //B
+       int q = 55;
+       int qw = 9;
+       int qwe = q + qw;
+       int qwer = qwe / 9;
+       System.out.println(qwer);
+       //C
+        int w = 20;
+        int we = -3;
+        int wer = 5;
+        int wert = 8;
+        int werty = we * wer;
+        int wertyu = werty / wert;
+        int wertyui= w + wertyu;
+        System.out.println(wertyui);
+        //D
+        int e = 5;
+        int er = 15;
+        int ert = 3;
+        int erty = 2;
+        int ertyu = 8;
+        int ertyuio = er / ert;
+        int ertyuiop = ertyu / ert;
+        int prealung = ertyuio * 2;
+        int prealung1 = 5 + prealung;
+        int prealung2 = prealung1 - ertyuiop;
+        System.out.println(prealung2);
+    }
+
+
+
+}
